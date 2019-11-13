190a54140c54848392c52acad5a86b668f0c23be
Janne Enberg <janne.enberg@lietu.net>
2019-11-13 23:04:45 +0200

    Fixed issues with Non-Interactive mode in Enable Hyper-V recipe

--- recipes/enable-hyper-v.ps1
+++ recipes/enable-hyper-v.ps1
@@ -1,5 +1,5 @@
 # Enable Hyper-V feature
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoReboot
 
 # Make current user a Hyper-V Administrator to avoid issues with e.g. minikube
 net localgroup "Hyper-V Administrators" $env:USERNAME /add
