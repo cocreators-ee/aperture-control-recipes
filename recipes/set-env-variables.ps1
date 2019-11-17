4599c8092748d5bdce6e1a3b15dbb3fb41833262
Janne Enberg <janne.enberg@lietu.net>
2019-11-17 11:30:46 +0200

    Improved env variable handling

--- recipes/set-env-variables.ps1
+++ recipes/set-env-variables.ps1
@@ -1,3 +1,12 @@
 # E.g. if you set GIT_SSH variable to C:\ProgramData\chocolatey\bin\PLINK.EXE it will
 # make Git use Pageant from Chocolatey for SSH keys:
-# New-Item -Path env:\GIT_SSH -Value C:\ProgramData\chocolatey\bin\PLINK.EXE
+# set_env GIT_SSH C:\ProgramData\chocolatey\bin\PLINK.EXE
+
+function set_env([string]$name, $value) {
+    $path = "env:\$name"
+    if (Get-Item -Path $path) {
+        Set-Item -Path $path -Value $value
+    } else {
+        New-Item -Path $path -Value $value
+    }
+}
