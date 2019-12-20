d3b100e5f7fa96f2da534b0a89b390f83067164b
Janne Enberg <janne.enberg@lietu.net>
2019-12-20 18:46:17 +0200

    Fixing Chocolatey unattended upgrade

--- recipes/install-chocolatey.ps1
+++ recipes/install-chocolatey.ps1
@@ -2,7 +2,7 @@
 
 if (Get-Command choco.exe -ErrorAction SilentlyContinue) {
     Write-Output "Chocolatey already installed, upgrading instead."
-    choco.exe upgrade chocolatey
+    choco.exe upgrade -y chocolatey
 } else {
     Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
