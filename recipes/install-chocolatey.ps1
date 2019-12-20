f7682554bc7afd7c7be9b0c1da8c71e706e76a09
Janne Enberg <janne.enberg@lietu.net>
2019-12-20 17:54:05 +0200

    Write-Output is loggable

--- recipes/install-chocolatey.ps1
+++ recipes/install-chocolatey.ps1
@@ -1,7 +1,7 @@
 # Install or upgrade Chocolatey package manager
 
 if (Get-Command choco.exe -ErrorAction SilentlyContinue) {
-    Write-Host "Chocolatey already installed, upgrading instead."
+    Write-Output "Chocolatey already installed, upgrading instead."
     choco.exe upgrade chocolatey
 } else {
     Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
--- recipes/install-scoop.ps1
+++ recipes/install-scoop.ps1
@@ -1,7 +1,7 @@
 # Install or upgrade Scoop package manager
 
 if (Get-Command scoop.cmd -ErrorAction SilentlyContinue) {
-    Write-Host "Scoop already installed, upgrading instead."
+    Write-Output "Scoop already installed, upgrading instead."
     scoop.cmd update scoop
 } else {
     Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
